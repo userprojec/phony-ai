98eab3564177e029630c1988faa2eb1aaa91884e
UserProjec <164155603+userprojec@users.noreply.github.com>
2026-09-07 14:14:31 +0545

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.VITE_BASE || '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    target: 'es2015',
+    outDir: 'dist/client',
+    emptyOutDir: true,
+  },
+})
